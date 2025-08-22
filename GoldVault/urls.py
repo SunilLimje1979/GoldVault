9998f80385727b86b8897e35c3b14e4c1f63ceea
Pawarshekhar709 <pawarshekhar708@gmail.com>
2025-08-22 15:12:00 +0530

    first commit

--- GoldVault/urls.py
+++ GoldVault/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('GoldVaultApp.urls')),
+    path('GoldVault/admin/', admin.site.urls),
+    path('GoldVault/', include('GoldVaultApp.urls')),
 ]
 
 
